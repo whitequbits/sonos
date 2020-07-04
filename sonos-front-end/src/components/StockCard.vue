36aa0bd6997c59ec158dec78b98ea7169261325b
rainoverme002 <bagus.gitdata@gmail.com>
2020-07-04 11:59:51 +0700

    Add Stock Detail Page

---
+++ sonos-front-end/src/components/StockCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      {{symbol}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'StockCard',
+  props: {
+    symbol: String,
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+    margin-top: 2%;
+    margin-bottom: 2%;
+    width: 75%;
+  }
+</style>
--- sonos-front-end/src/views/StockDetail.vue
+++ sonos-front-end/src/views/StockDetail.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="stock-detail">
-    {{ $route.params.symbol }}
+    <div class="container">
+      <StockCard class="mx-auto" :symbol="$route.params.symbol"/>
+      <Table class="mx-auto"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Table from '@/components/Table'
+import StockCard from '@/components/StockCard'
 
 export default {
   name: 'StockDetail',
+  components: { 
+    Table,
+    StockCard
+  },
   props: {
     symbol: String
   }
@@ -15,5 +24,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .container {
+    margin-top: 2%;
+  }
 </style>
