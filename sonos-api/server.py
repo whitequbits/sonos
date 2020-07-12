e1cff67c87c3fecde1f54843de0203fe234b644e
rainoverme002 <bagus.gitdata@gmail.com>
2020-07-12 16:16:03 +0700

    Add Procfile for heroku

--- sonos-api/server.py
+++ sonos-api/server.py
@@ -19,7 +19,7 @@ def fastapi_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
